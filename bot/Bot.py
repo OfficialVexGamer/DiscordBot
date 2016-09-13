0c0068617ccc1c9ffb1c648861e60c2afa65d51b
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-13 21:56:00 +0300

    avatar feature

--- bot/Bot.py
+++ bot/Bot.py
@@ -4,6 +4,7 @@ from bot.commands.mute import MuteCommand
 from bot.commands.unmute import UnmuteCommand
 import discord
 import asyncio
+import os
 
 client = discord.Client()
 commands = {
@@ -16,7 +17,16 @@ commands = {
 
 @client.event
 async def on_ready():
-    print("[OK] Ready! " + client.user.name + " " + client.user.id)
+    if not os.path.exists(".avatar_uploaded"):
+        with open(cfg["avatar"], 'rb') as f:
+            print("Uploading avatar...")
+            await client.edit_profile(avatar=f.read())
+            with open(".avatar_uploaded", 'w') as f2:
+                f2.write(".")
+    else:
+        print("Avatar already uploaded (wanna change? remove .avatar_uploaded)")
+
+    print("Ready! " + client.user.name + " " + client.user.id)
 
 
 @client.event
@@ -28,7 +38,7 @@ async def on_message(message):
         cmd = message.content[1:].split()[0]
         c_args = message.content[1:].split()[1:]
 
-        print("[OK] Komut: (" + message.author.name + ") " + cmd + " args: " + str(c_args))
+        print("Komut: (" + message.author.name + ") " + cmd + " args: " + str(c_args))
 
         if not commands.get(cmd):
             await client.send_message(message.channel, "@" + message.author.name + " Bu komut yok!")
@@ -51,6 +61,6 @@ def start(config):
     global cfg
     cfg = config
 
-    print("[OK] Starting...")
+    print("Starting...")
     client.run(config["token"])
     return 0
