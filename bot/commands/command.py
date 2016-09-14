a01989653dbde03809fa87aa995f5afda36defa7
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-14 14:57:42 +0300

    help command

--- bot/commands/command.py
+++ bot/commands/command.py
@@ -6,8 +6,10 @@ class Command:
         return False
 
     async def do(self, client, message, args, config={}):
-        await client.send_message(message.channel, "Bot 1.0.0! ( by @admicos )")
-        await client.send_message(message.channel, "Kaynak: admicos.cf/s?EayKf")
-        await client.send_message(message.channel, "Komutlar: ")
-        await client.send_message(message.channel, "!cleverbot <mesaj>")
-        await client.send_message(message.channel, "!i <resim>")
+        await client.send_message(message.channel, """```
+Bot 1.0.0! ( by @admicos )
+Kaynak: https://admicos.cf/s?EayKf
+Komutlar:
+!cleverbot <mesaj>
+!i <resim>
+```""")
