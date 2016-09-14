743fd000f04986e7fe791d9379bd04e7a05ad754
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-14 14:55:23 +0300

    help command

--- bot/commands/command.py
+++ bot/commands/command.py
@@ -7,7 +7,7 @@ class Command:
 
     async def do(self, client, message, args, config={}):
         await client.send_message(message.channel, "Bot 1.0.0! ( by @Admicos )")
-        await client.send_message(message.channel, "Source: admicos.cf/s?EayKf")
+        await client.send_message(message.channel, "Kaynak: admicos.cf/s?EayKf")
         await client.send_message(message.channel, "Komutlar: ")
         await client.send_message(message.channel, "!cleverbot <mesaj>")
         await client.send_message(message.channel, "!i <resim>")
