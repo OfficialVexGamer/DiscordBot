d7afccf4440e0bbc3d90dbde76295d0ed7ff01ee
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-14 21:29:55 +0300

    new opts

--- bot/Bot.py
+++ bot/Bot.py
@@ -55,7 +55,7 @@ async def on_message(message):
             if message.content.startswith('!id '):
                 msgChan = str(message.content[4:])
             elif message.content.startswith("!name "):
-                await client.edit_profile(username=str(message.content[6]))
+                await client.edit_profile(username=str(message.content[6:]))
             else:
                 await client.send_message(client.get_channel(msgChan), message.content)
 
