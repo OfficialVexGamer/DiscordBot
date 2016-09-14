5c2bdf44f35fb546f1a2dd5cb4e1b0cd7a2ac4fb
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-14 21:14:28 +0300

    cid change

--- bot/Bot.py
+++ bot/Bot.py
@@ -53,6 +53,7 @@ async def on_message(message):
             global msgChan
 
             if message.content.startswith('!id '):
+                print(msgChan)
                 msgChan = str(message.content[3:])
             else:
                 await client.send_message(client.get_channel(msgChan), message.content)
