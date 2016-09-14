011d36a208f64a027b6c5e8eafcda55a92ceb780
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-14 16:03:52 +0300

    channel locking

--- bot/commands/lock.py
+++ bot/commands/lock.py
@@ -22,5 +22,5 @@ class UnlockCommand(Command):
         return True
 
     async def do(self, client, message, args, config={}):
-        await client.send_message(message.channel, message.author.mention + " Kanal kilidini açtı.")
         muted_chans[message.channel.name] = False
+        await client.send_message(message.channel, message.author.mention + " Kanal kilidini açtı.")
