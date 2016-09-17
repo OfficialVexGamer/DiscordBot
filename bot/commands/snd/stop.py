06aa3a32bbcb5ad27d3e8bb2c78d5c272dfbea0d
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-17 18:10:49 +0300

    that was easy

--- bot/commands/snd/stop.py
+++ bot/commands/snd/stop.py
@@ -2,7 +2,7 @@ from bot.commands.command import Command
 from bot import stuff
 
 
-class SoundVolCommand(Command):
+class SoundStopCommand(Command):
     def requiresAdmin(self):
         return True
 
@@ -10,12 +10,11 @@ class SoundVolCommand(Command):
         return True
 
     def command(self):
-        return "snd_vol"
+        return "snd_stop"
 
     async def do(self, client, message, args, config={}):
         if not stuff.player:
             await client.send_message(message.channel, message.author.mention +
                                       " Lütfen önce !snd_play ile bir müzik açın!")
 
-        stuff.player.volume = float(args[0])
-        stuff.old_vol = float(args[0])
+       stuff.player.stop()
