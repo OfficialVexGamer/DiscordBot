e62401e9be0616cf3d4ca2327093b99a364b893b
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-22 16:17:47 +0000

    basic l18n setup

---
+++ bot/i18n.py
@@ -0,0 +1,17 @@
+import yaml
+
+local_strs = {}
+
+
+def load_lang(lang: str):
+    global local_strs
+
+    with open("lang/{}.yml".format(lang.lower()), "r", encoding="utf8") as f:
+        local_strs = yaml.load(f.read())
+
+
+def get_localized_str(str_id: str, fmt=()):
+    if not local_strs[str_id]:
+        return "STRING NOT FOUND: {}".format(str_id)
+
+    return str(local_strs[str_id]).format(fmt)
