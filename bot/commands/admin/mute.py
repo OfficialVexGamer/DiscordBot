5986b8deb0f2f6bdef2be25885378fb29fe2dee8
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-23 20:47:19 +0000

    mute.py translated

--- bot/commands/admin/mute.py
+++ bot/commands/admin/mute.py
@@ -1,3 +1,4 @@
+from bot import i18n
 from bot.commands.command import Command
 
 
@@ -13,17 +14,17 @@ class MuteCommand(Command):
 
     async def do(self, client, message, args, config={}):
         if len(args) < 1:
-            await client.send_message(message.channel, message.author.mention + " !mute <isim>")
+            await client.send_message(message.channel, i18n.get_localized_str("cmd_mute_help"))
             return
 
         if not config["mute_role"]:
-            await client.send_message(message.channel, message.author.mention +
-                                      ": Bot Konfigurasyon hatası! (mute_role bulunamadı)")
+            await client.send_message(message.channel, i18n.get_localized_str("bot_config_error", {"cmd": self.command(),
+                                                                                                   "key": "mute_role"}))
             return
 
         if not config["admin_roles"]:
-            await client.send_message(message.channel, message.author.mention +
-                                      ": Bot Konfigurasyon hatası! (admin_roles bulunamadı)")
+            await client.send_message(message.channel, i18n.get_localized_str("bot_config_error", {"cmd": self.command(),
+                                                                                                   "key": "admin_roles"}))
             return
 
         nameToMute = ""
@@ -35,17 +36,20 @@ class MuteCommand(Command):
                 for role in member.roles:
                     for check_role in config["admin_roles"]:
                         if role.name == check_role:
-                            await client.send_message(message.channel, message.author.mention +
-                                                      " Bir admini muteleyemessin!")
+                            await client.send_message(message.channel, i18n.get_localized_str("cmd_mute_admin"))
                             return
 
                 for role in message.server.roles:
                     if role.name.lower() == config["mute_role"].lower():
                         await client.add_roles(member, role)
-                        await client.send_message(message.channel, member.mention + " Susturuldu")
+                        await client.send_message(message.channel, i18n.get_localized_str("cmd_mute", {"mention":
+                                                                                                       member.mention}))
                         return
 
-                await client.send_message(message.channel, message.author.mention + " Mute rolü yok!")
+                await client.send_message(message.channel, i18n.get_localized_str("bot_serv_cfg_error", {
+                    "cmd": self.command(),
+                    "err": i18n.get_localized_str("err_nomuterole")
+                }))
                 return
 
-        await client.send_message(message.channel, message.author.mention + " Böyle bir kişi yok!")
+        await client.send_message(message.channel, i18n.get_localized_str("cmd_mute_notfound", {"name": nameToMute}))
