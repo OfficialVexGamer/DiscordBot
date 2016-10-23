ff15b82e6a105c12924b647ba0e0b06da324d970
Ecmel B. CANLIER <git@admicos.cf>
2016-10-23 10:36:44 +0000

    up the version

--- bot/stuff.py
+++ bot/stuff.py
@@ -9,7 +9,7 @@ msgChan = "225218131537297408"
 # noinspection PyUnresolvedReferences
 commands = inspect.getmembers(bot.commands, predicate=lambda o: inspect.isclass(o) and issubclass(o, Command))
 timeout = {}
-bot_version = "1.2.0"
+bot_version = "1.2.1"
 
 def find_cmd_class(cmd: str):
     cmd_c = NoneCommand()
