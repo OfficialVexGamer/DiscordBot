4d6d3d0b1fea61126f6956c51ab080b1d369a17d
Ecmel B. CANLIER <git@admicos.cf>
2017-01-13 17:56:20 +0300

    make mute/unmute use id instead of name

--- bot/commands/admin/mute.py
+++ bot/commands/admin/mute.py
@@ -34,36 +34,41 @@ class MuteCommand(Command):
                                                                                                    "key": "admin_roles"}))
             return
 
-        nameToMute = args[0]
+        idToMute = (args[0])
         muteReason = i18n.get_localized_str(message.server.id, "cmd_unmute_emptyreason")
 
         if len(args) > 1:
             muteReason = args[1]
 
-        for member in message.channel.server.members:
-            if nameToMute.lower().strip() == member.name.lower():
-                for role in member.roles:
-                    for check_role in config.get_key(message.server.id, "admin_roles"):
-                        if role.name == check_role:
-                            await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute_admin"))
-                            return
+        member = message.server.get_member(idToMute)
+        if not member:
+            await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute_notfound", {
+                idToMute
+            }))
+            return
 
-                for role in message.server.roles:
-                    if role.name.lower() == config.get_key(message.server.id, "mute_role").lower():
-                        await client.add_roles(member, role)
-                        await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute", {"mention":
-                                                                                                       member.mention}))
-                        return i18n.get_localized_str(message.server.id, "cmd_mute_log", {
-                                "name": member.name,
-                                "reason": muteReason,
-                                "responsible": message.author.name,
-                                "date": datetime.datetime.today().strftime('%Y-%m-%d %H:%M')
-                            })
+        for role in member.roles:
+            for check_role in config.get_key(message.server.id, "admin_roles"):
+                if role.name == check_role:
+                    await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute_admin"))
+                    return
 
-                await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "bot_serv_cfg_error", {
-                    "cmd": self.command(),
-                    "err": i18n.get_localized_str(message.server.id, "err_nomuterole")
-                }))
-                return
+        for role in message.server.roles:
+            if role.name.lower() == config.get_key(message.server.id, "mute_role").lower():
+                await client.add_roles(member, role)
+                await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute", {"mention":
+                                                                                               member.mention}))
+                return i18n.get_localized_str(message.server.id, "cmd_mute_log", {
+                        "name": member.name,
+                        "reason": muteReason,
+                        "responsible": message.author.name,
+                        "date": datetime.datetime.today().strftime('%Y-%m-%d %H:%M')
+                    })
+
+        await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "bot_serv_cfg_error", {
+            "cmd": self.command(),
+            "err": i18n.get_localized_str(message.server.id, "err_nomuterole")
+        }))
+        return
 
         await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute_notfound", {"name": nameToMute}))
--- bot/commands/admin/unmute.py
+++ bot/commands/admin/unmute.py
@@ -34,29 +34,34 @@ class UnmuteCommand(Command):
                                                                                                    "key": "admin_roles"}))
             return
 
-        nameToMute = args[0]
+        idToMute = int(args[0])
         muteReason = i18n.get_localized_str(message.server.id, "cmd_unmute_emptyreason")
 
         if len(args) > 1:
             muteReason = args[1]
 
-        for member in message.server.members:
-            if nameToMute.lower().strip() == member.name.lower():
-                for role in member.roles:
-                    for check_role in config.get_key(message.server.id, "admin_roles"):
-                        if role.name == check_role:
-                            await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute_admin"))
-                            return
-                        elif role.name == config.get_key(message.server.id, "mute_role"):
-                            await client.remove_roles(member, role)
-                            await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_unmute", {
-                                "mention": member.mention
-                            }))
-                            return i18n.get_localized_str(message.server.id, "cmd_unmute_log", {
-                                "name": member.name,
-                                "reason": muteReason,
-                                "responsible": message.author.name,
-                                "date": datetime.datetime.today().strftime('%Y-%m-%d %H:%M')
-                            })
+        member = message.server.get_member(idToMute)
+        if not member:
+            await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute_notfound", {
+                idToMute
+            }))
+            return
+
+        for role in member.roles:
+            for check_role in config.get_key(message.server.id, "admin_roles"):
+                if role.name == check_role:
+                    await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute_admin"))
+                    return
+                elif role.name == config.get_key(message.server.id, "mute_role"):
+                    await client.remove_roles(member, role)
+                    await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_unmute", {
+                        "mention": member.mention
+                    }))
+                    return i18n.get_localized_str(message.server.id, "cmd_unmute_log", {
+                        "name": member.name,
+                        "reason": muteReason,
+                        "responsible": message.author.name,
+                        "date": datetime.datetime.today().strftime('%Y-%m-%d %H:%M')
+                    })
 
         await client.send_message(message.channel, i18n.get_localized_str(message.server.id, "cmd_mute_notfound", {"name": nameToMute}))
